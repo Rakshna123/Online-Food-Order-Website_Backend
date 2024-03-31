166e3a33461e8799152b456cb5f6cfabee867c36
Rakshna123 <97587284+Rakshna123@users.noreply.github.com>
2024-03-31 22:37:44 +0530

    Update diy62.js

--- diy62.js
+++ diy62.js
@@ -3,19 +3,19 @@ var app = express();
 var port = 8300;
 var cors = require("cors");
 var mongoose = require("mongoose");
-app.use(cors())
+app.use(cors());
 var router = require("./router.js");
 app.use(express.json());
 
-app.use("/",router);
+app.use("/", router);
 
-
-mongoose.connect("mongodb+srv://rakshnakb:MongoDBatlas@cluster0.6ybz5av.mongodb.net/",{useNewUrlParser:true})
-.then(success=>{
-    console.log("connected");
-    app.listen(port,()=>{
-        console.log("server is running");
+mongoose.connect("mongodb+srv://rakshnakb:MongoDBatlas@cluster0.6ybz5av.mongodb.net/")
+  .then(success => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log("Server is running");
     });
-}).catch(error=>{
-    console.log("error occur");
-});
+  })
+  .catch(error => {
+    console.error("Error connecting to MongoDB:", error);
+  });
